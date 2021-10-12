e924defd0ceac4a401e7c1b9fc980931d4e496a3
Artonit Kameri <artonitkameri8@gmail.com>
2021-10-12 13:38:26 +0200

    JS-HomeWork

---
+++ ora 7/js/main.js
@@ -0,0 +1,24 @@
+// var firstName = "Artonit",
+//   lastName = "Kameri";
+
+// function numberCalculate(number1, number2) {
+//   return number1 + number2;
+// }
+
+// console.log(numberCalculate(2, 3));
+// console.log(numberCalculate(4, 5));
+// console.log(numberCalculate(7, 8));
+
+var name = prompt("Please enter your name:");
+console.log("Hello " + name);
+
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
+
+console.log(width, height);
