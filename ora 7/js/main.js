0d24e804c962c7dc2385202d8d363f2f047fcc58
Artonit Kameri <artonitkameri8@gmail.com>
2021-10-14 13:45:28 +0200

    homework-25

--- ora 7/js/main.js
+++ ora 7/js/main.js
@@ -22,3 +22,55 @@ const height =
   document.body.clientHeight;
 
 console.log(width, height);
+
+alert("Hello" + name);
+
+var confirmValue = confirm("Are you sure you want to discard this action?");
+
+console.log(confirmValue);
+
+// document.getElementById("demo").innerHTML = Math.round(4.4);
+
+// console.log("demo");
+
+let x = 5;
+let y = 2;
+let z = x * y;
+console.log(z);
+
+let a = 30;
+console.log(a);
+
+let k = 5;
+let t = 5;
+let r = k * t;
+console.log(r);
+
+var q = 10;
+q += 5;
+
+console.log(q);
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+
+console.log(text3);
+
+let text4 = "What a very ";
+text4 += "nice day";
+console.log(text4);
+
+let sqrt = Math.sqrt(9);
+document.write(sqrt);
+
+console.log(sqrt);
+
+document.getElementById("demo").innerHTML = Math.round(4.4);
+document.getElementById("demo").innerHTML = Math.pow(8, 2);
+document.getElementById("demo").innerHTML = Math.sqrt(64);
+document.getElementById("demo").innerHTML = Math.random();
+console.log(Math.round(4.4));
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(64));
+console.log(Math.random());
