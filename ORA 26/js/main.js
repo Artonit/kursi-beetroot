ad6be66a27485221859d90507078ebfaacc83082
Artonit Kameri <artonitkameri8@gmail.com>
2021-10-18 23:10:58 +0200

    homework

---
+++ ORA 26/js/main.js
@@ -0,0 +1,61 @@
+function findTheEnigma(number) {
+  var enigma = 100;
+
+  if (!number) {
+    return "Shkruaj nje numer";
+  }
+  if (number < enigma / 2) {
+    return "Vlera e enigmes eshte oer te pakten dy her me e madhe";
+  }
+  if (number * 2 > enigma) {
+    return "Vlera e enigmes eshte per te pakten dy her me e vogel";
+  }
+  if (number == enigma) {
+    return "Vlera e enigmes eshte e sakt";
+  }
+  return "Vlera e enigmes nuk eshte qelluar";
+}
+console.log(findTheEnigma(30));
+
+function colorMessage(color) {
+  if (!color) {
+    return "Zgjedh nje ngjyre";
+  }
+  if (color === "red") {
+    return "Message for red color";
+  } else if (color === "blue") {
+    return "Message for blue color";
+  } else if (color === "yellow") {
+    return "Message for yellow color";
+  } else if (color === "green") {
+    return "Message for green color";
+  } else {
+    return "Nuk ka ndonje mesazh per ngjyren";
+  }
+}
+
+console.log(colorMessage("green"));
+
+function colorMessageSwitch(color) {
+  switch (color) {
+    case "!color":
+      console.log("Zgjedh nje ngjyre");
+      break;
+    case "red":
+      console.log("red");
+      break;
+    case "blue":
+      console.log("blue");
+      break;
+    case "yellow":
+      console.log("yellow");
+      break;
+    case "green":
+      console.log("green");
+      break;
+    default:
+      alert("Nuk ka ndonje mesazh per ngjyren");
+      break;
+  }
+}
+console.log(colorMessageSwitch("yellow"));
--- ora 7/js/main.js
+++ ora 7/js/main.js
@@ -9,68 +9,124 @@
 // console.log(numberCalculate(4, 5));
 // console.log(numberCalculate(7, 8));
 
-var name = prompt("Please enter your name:");
-console.log("Hello " + name);
+// var name = prompt("Please enter your name:");
+// console.log("Hello " + name);
 
-const width =
-  window.innerWidth ||
-  document.documentElement.clientWidth ||
-  document.body.clientWidth;
-const height =
-  window.innerHeight ||
-  document.documentElement.clientHeight ||
-  document.body.clientHeight;
+// function getWindowWidth() {
+//   return "width:" + window.innerWidth;
+// }
 
-console.log(width, height);
+// console.log(getWindowWidth());
 
-alert("Hello" + name);
+// const width =
+//   window.innerWidth ||
+//   document.documentElement.clientWidth ||
+//   document.body.clientWidth;
+// const height =
+//   window.innerHeight ||
+//   document.documentElement.clientHeight ||
+//   document.body.clientHeight;
 
-var confirmValue = confirm("Are you sure you want to discard this action?");
+// console.log(width, height);
 
-console.log(confirmValue);
+// alert("Hello" + name);
+
+// var confirmValue = confirm("Are you sure you want to discard this action?");
+
+// console.log(confirmValue);
 
 // document.getElementById("demo").innerHTML = Math.round(4.4);
 
 // console.log("demo");
 
-let x = 5;
-let y = 2;
-let z = x * y;
-console.log(z);
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z);
 
-let a = 30;
-console.log(a);
+// let a = 30;
+// console.log(a);
 
-let k = 5;
-let t = 5;
-let r = k * t;
-console.log(r);
+// let k = 5;
+// let t = 5;
+// let r = k * t;
+// console.log(r);
 
-var q = 10;
-q += 5;
+// var q = 10;
+// q += 5;
 
-console.log(q);
+// console.log(q);
 
-let text1 = "John";
-let text2 = "Doe";
-let text3 = text1 + " " + text2;
+// let text1 = "Artonit";
+// let text2 = "Kameri";
+// let text3 = text1 + " " + text2;
 
-console.log(text3);
+// console.log(text3);
 
-let text4 = "What a very ";
-text4 += "nice day";
-console.log(text4);
+// let text4 = "ku tkom bre ";
+// text4 += "qe sa stpasna pa";
+// console.log(text4);
 
-let sqrt = Math.sqrt(9);
-document.write(sqrt);
+// let sqrt = Math.sqrt(9);
+// document.write(sqrt);
 
-console.log(sqrt);
+// console.log(sqrt);
 
-document.getElementById("demo").innerHTML = Math.round(4.4);
-document.getElementById("demo").innerHTML = Math.pow(8, 2);
-document.getElementById("demo").innerHTML = Math.sqrt(64);
-document.getElementById("demo").innerHTML = Math.random();
-console.log(Math.round(4.4));
-console.log(Math.pow(8, 2));
-console.log(Math.sqrt(64));
-console.log(Math.random());
+// document.getElementById("demo").innerHTML = Math.round(4.4);
+// document.getElementById("demo").innerHTML = Math.pow(8, 2);
+// document.getElementById("demo").innerHTML = Math.sqrt(64);
+// document.getElementById("demo").innerHTML = Math.random();
+// console.log(Math.round(4.4));
+// console.log(Math.pow(8, 2));
+// console.log(Math.sqrt(64));
+// console.log(Math.random());
+// var firstNumber = 10;
+
+// if (firstNumber > 40) {
+//   console.log("pass!");
+// } else if (firstNumber > 5) {
+//   console.log("second if passed!");
+// } else {
+//   console.log("canceled!");
+// }
+
+var number = 10;
+
+switch (number) {
+  case 5:
+    console.log("numri5");
+    break;
+  case 10:
+    console.log("numri10");
+    break;
+  case 20:
+    console.log("numri20");
+    break;
+  case 30:
+    console.log("numri30");
+    break;
+  default:
+    alert("nuk bon");
+    break;
+}
+
+function colorMessage(color) {
+  if (!color) {
+    return "Please provide a color";
+  }
+  if (color === "red") {
+    return "Message for red color";
+  } else if (color === "blue") {
+    return "Message for blue color";
+  } else if (color === "yellow") {
+    return "Message for yellow color";
+  } else if (color === "green") {
+    return "Message for green color";
+  } else {
+    return "unknown message";
+  }
+}
+
+console.log(colorMessage());
+console.log(colorMessage("blue"));
+console.log(colorMessage("yellow"));
